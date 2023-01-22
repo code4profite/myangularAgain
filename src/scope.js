a43aad085ec78dfaf22813a6e64372128282f625
sheratsuki-mac <mohamed.hizam111@gmail.com>
2023-01-22 03:47:32 +0100

    listenerFn call incomplete, go back to dell pc azerty of mac dosen't have brackets of array

--- src/scope.js
+++ src/scope.js
@@ -1,3 +1,20 @@
-module.exports = function Scope() {
-    
+var _ = require('lodash');
+
+function Scope() {
+    this.$$watchers = Array();
+};
+module.exports = Scope;
+
+Scope.prototype.$watch = function (watchFn,listenerFn) {
+    var watcher = {
+        watchFn:watchFn,
+        listenerFn:listenerFn
+    };
+    Scope.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function () {
+    _.forEach(this.$$watchers,function (watch) {
+        watch.listenerFn();
+    })
 }
